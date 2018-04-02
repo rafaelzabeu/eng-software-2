33b6f83a768ea94a3397d13f885846f854ac4720
rafaelzabeu <rafaelzabeu@gmail.com>
2018-04-02 16:49:39 -0300

    Minha parte

--- src/TesteJogo.java
+++ src/TesteJogo.java
@@ -1,14 +1,37 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TesteJogo {
 
-	@Test()
+	private Jogo jogo;
+	private Participante part1;
+	
+	@BeforeEach
+	public void SetUp()
+	{
+		jogo = new Jogo("Joguinho");
+		part1 = new Participante("Ronaldo");
+	}
+	
+	//Rafael Zabeu
+	@Test
+	void naoPodeTerDoisResultadosDoMesmoJogador()
+	{
+		jogo.anota(new Resultado(part1, 10));
+		jogo.anota(new Resultado(part1, 20));
+		
+		assertEquals(1, jogo.getResultados().size());
+	}
+	
+	//Rafael Sançon
+	@Test
 	void quandoUmJogoForEncerradoComMenosDeDoisResultadosEntaoUmaExcecaoSeraDisparada() {
-		Jogo jogo = new Jogo("Joguinho");
-		jogo.anota(new Resultado(new Participante("Ronaldo"), 20.0));
-		assertThrows(NullPointerException.class,
+		jogo.anota(new Resultado(part1, 20.0));
+		assertThrows(Exception.class,
 				()->{
 					jogo.encerrar(); 
 				});
